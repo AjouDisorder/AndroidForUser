279c52340357dfefba50c5d11af60ffbe96d931e
MTG <rjwltps57@ajou.ac.kr>
2020-06-20 11:13:11 +0900

    correct bugg

--- AndroidForUser/app/src/main/java/com/example/ttruserver2/MyFirebaseMessagingService.kt
+++ AndroidForUser/app/src/main/java/com/example/ttruserver2/MyFirebaseMessagingService.kt
@@ -14,16 +14,17 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if(remoteMessage?.notification != null){
+            sendNotification(remoteMessage.notification?.title,remoteMessage.notification?.body)
 
         }
     }
-    private fun sendNotification(title: String?, body: String){
+    private fun sendNotification(title: String?, body: String?){
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this)
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(title)
             .setContentText(body)
             .setSound(defaultSound)
