9af881d4046df37f709a1e9bac26485bd04d70cb
MTG <52411740+rjwltpsw57@users.noreply.github.com>
2020-06-20 20:23:04 +0900

    correct favorite tab (#28)

--- AndroidForUser/app/src/main/java/com/example/ttruserver2/BottomTab/Coupon/BottomCouponActivity.kt
+++ AndroidForUser/app/src/main/java/com/example/ttruserver2/BottomTab/Coupon/BottomCouponActivity.kt
@@ -1,17 +1,16 @@
 package com.example.ttruserver2.BottomTab.Coupon
 
 import android.content.Intent
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.ttruserver2.*
 import com.example.ttruserver2.BottomTab.UserInfo.BottomMyInfoActivity
-import com.example.ttruserver2.BottomTab.FavoriteRestaurant.BottomFavoriteRestaurantActivity
-import com.example.ttruserver2.LogInActivity
-import com.example.ttruserver2.MainActivity
-import com.example.ttruserver2.R
 import com.example.ttruserver2.Retrofit.IMyService
 import com.example.ttruserver2.Retrofit.RetrofitClient
-import com.example.ttruserver2.UserData
+import com.example.ttruserver2.models.OriginMenuModel
+import com.example.ttruserver2.models.SearchedRestaurantModel
 import com.example.ttruserver2.models.TicketModel
 import kotlinx.android.synthetic.main.activity_bottom_coupon.*
 import kotlinx.android.synthetic.main.bottom.*
@@ -102,9 +101,67 @@ class BottomCouponActivity : AppCompatActivity() {
                 val loginIntent = Intent(this, LogInActivity::class.java)
                 startActivity(loginIntent)
             }else{
-                val intent = Intent(this, BottomFavoriteRestaurantActivity::class.java)
-                startActivity(intent)
-                finish()
+                iMyService.getFavoriteList(UserData.getOid().toString()).enqueue(object :
+                    Callback<ResponseBody> {
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        Toast.makeText(this@BottomCouponActivity, "Fail : $t", Toast.LENGTH_SHORT).show()
+                    }
+                    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                        val favoriteRestaurantModelList = arrayListOf<SearchedRestaurantModel>()
+                        val result = response.body()?.string()
+                        val jsonArray = JSONArray(result)
+
+                        for (i in 0.until(jsonArray.length())){
+                            val jsonObject: JSONObject = jsonArray.getJSONObject(i)
+
+                            val _id = jsonObject.getString("_id")
+                            val type = jsonObject.getString("type")
+                            val title = jsonObject.getString("title")
+                            val grade = jsonObject.getDouble("avrGrade")
+
+                            var onSale = true
+                            if (jsonObject.getJSONArray("menuidList").length() == 0){
+                                onSale = false
+                            }
+                            val favoriteCount = jsonObject.getInt("favoriteCount")
+                            val description = jsonObject.getString("description")
+                            val address = jsonObject.getString("address")
+                            val phone = jsonObject.getString("phone")
+
+                            var originMenuList =  arrayListOf<OriginMenuModel>()
+                            val originMenuJson = jsonObject.getJSONArray("originMenuList")
+                            for(i in 0.until(originMenuJson.length())){
+                                val originMenu: JSONObject = originMenuJson.getJSONObject(i)
+                                val originMenuTitle = originMenu.getString("title")
+                                val originMenuPrice = originMenu.getInt("originPrice")
+                                originMenuList.add(OriginMenuModel(originMenuTitle, originMenuPrice))
+                            }
+
+                            val lng = jsonObject.getJSONObject("location").getJSONArray("coordinates").get(0)
+                            val lat= jsonObject.getJSONObject("location").getJSONArray("coordinates").get(1)
+
+                            val userLocation = Location("userPoint")
+                            userLocation.latitude = UserData.getLat() as Double
+                            userLocation.longitude = UserData.getLng() as Double
+                            val restaurantLocation = Location("restaurantPoint")
+                            restaurantLocation.latitude = lat as Double
+                            restaurantLocation.longitude = lng as Double
+                            val distance : Double = Math.round((userLocation.distanceTo(restaurantLocation)).toDouble()/100.0)/10.0
+
+                            if(_id != null){
+                                favoriteRestaurantModelList.add(
+                                    SearchedRestaurantModel(_id, type, title, grade,
+                                        distance, onSale, favoriteCount, description, address, phone, originMenuList,
+                                        lng, lat)
+                                )
+                            }
+                        }
+                        val intent = Intent(this@BottomCouponActivity, SearchedRestaurantListActivity::class.java)
+                        intent.putExtra("searchedRestaurantModelList", favoriteRestaurantModelList)
+                        startActivity(intent)
+                    }
+
+                })
             }
         }
         bottom_tab_coupon.setOnClickListener {
